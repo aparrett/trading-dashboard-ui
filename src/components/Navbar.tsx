488cbe4e5f990e7b4bde50c769e264a26f190104
Anthony <anthonyparrett7@gmail.com>
2021-07-21 12:09:22 -0500

    Logout and retrieve user on load

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,60 @@
 import { Box } from '@material-ui/core'
-import { FC, useState } from 'react'
-import { useAuthState } from '../context/AuthContext'
+import { FC, useEffect, useState } from 'react'
+import { useAuthDispatch, useAuthState } from '../context/AuthContext'
+import { useLogoutMutation, useMeQuery } from '../generated/graphql'
 import RegisterDialog from './RegisterDialog'
 
 const Navbar: FC = () => {
     // const [showLoginDialog, setShowLoginDialog] = useState(false)
     const [showRegisterDialog, setShowRegisterDialog] = useState(false)
+    const [logout] = useLogoutMutation()
     const auth = useAuthState()
+    const dispatch = useAuthDispatch()
+
+    const [skip, setSkip] = useState(false)
+    const { data: meData } = useMeQuery({ skip })
+    useEffect(() => {
+        if (meData) {
+            // Once we get our response from the query, don't make any more requests.
+            setSkip(true)
+            if (meData.me) {
+                // If the user exists in the response, set it. Otherwise, the user will need to log in.
+                dispatch({ type: 'auth', payload: meData.me })
+            }
+        }
+    }, [meData, dispatch])
+
+    const handleLogout = async () => {
+        const res = await logout()
+        if (res?.data?.logout) {
+            dispatch({ type: 'logout' })
+        } else {
+            // TODO: use toast alert.
+            alert('Something went wrong.')
+        }
+    }
 
     return (
         <div>
             <Box display="flex" justifyContent="space-between" className="navbar">
                 <div className="logo">TRADE</div>
-                {auth.me ? (
-                    <div>{auth.me.username}</div>
-                ) : (
-                    <Box display="flex" justifyContent="flex-end">
-                        <div className="button link">LOGIN</div>
-                        <div className="button link" onClick={() => setShowRegisterDialog(true)}>
-                            REGISTER
-                        </div>
-                    </Box>
-                )}
+                <Box display="flex" justifyContent="flex-end">
+                    {auth.me ? (
+                        <>
+                            <div>{auth.me.username}</div>
+                            <div className="button link" onClick={handleLogout}>
+                                LOGOUT
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                            <div className="button link">LOGIN</div>
+                            <div className="button link" onClick={() => setShowRegisterDialog(true)}>
+                                REGISTER
+                            </div>
+                        </>
+                    )}
+                </Box>
             </Box>
             <RegisterDialog open={showRegisterDialog} onClose={() => setShowRegisterDialog(false)} />
         </div>
--- src/generated/graphql.tsx
+++ src/generated/graphql.tsx
@@ -71,6 +71,10 @@ export type UsernamePasswordInput = {
     password: Scalars['String']
 }
 
+export type LogoutMutationVariables = Exact<{ [key: string]: never }>
+
+export type LogoutMutation = { __typename?: 'Mutation' } & Pick<Mutation, 'logout'>
+
 export type RegisterMutationVariables = Exact<{
     options: UsernamePasswordInput
 }>
@@ -86,6 +90,37 @@ export type MeQueryVariables = Exact<{ [key: string]: never }>
 
 export type MeQuery = { __typename?: 'Query' } & { me?: Maybe<{ __typename?: 'User' } & Pick<User, 'id' | 'username'>> }
 
+export const LogoutDocument = gql`
+    mutation Logout {
+        logout
+    }
+`
+export type LogoutMutationFn = ApolloReactCommon.MutationFunction<LogoutMutation, LogoutMutationVariables>
+
+/**
+ * __useLogoutMutation__
+ *
+ * To run a mutation, you first call `useLogoutMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useLogoutMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [logoutMutation, { data, loading, error }] = useLogoutMutation({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useLogoutMutation(
+    baseOptions?: ApolloReactHooks.MutationHookOptions<LogoutMutation, LogoutMutationVariables>
+) {
+    return ApolloReactHooks.useMutation<LogoutMutation, LogoutMutationVariables>(LogoutDocument, baseOptions)
+}
+export type LogoutMutationHookResult = ReturnType<typeof useLogoutMutation>
+export type LogoutMutationResult = ApolloReactCommon.MutationResult<LogoutMutation>
+export type LogoutMutationOptions = ApolloReactCommon.BaseMutationOptions<LogoutMutation, LogoutMutationVariables>
 export const RegisterDocument = gql`
     mutation Register($options: UsernamePasswordInput!) {
         register(options: $options) {
