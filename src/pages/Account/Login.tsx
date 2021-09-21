0342ac362d2e497183f9791bbc94d25f30cbf950
Anthony <anthonyparrett7@gmail.com>
2021-09-20 22:02:12 -0500

    Fix the login form design

--- src/pages/Account/Login.tsx
+++ src/pages/Account/Login.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react'
 import { useFormik } from 'formik'
 import { MeQuery, MeDocument, useLoginMutation } from '../../generated/graphql'
-import { TextField } from '@material-ui/core'
+import { Grid, TextField } from '@material-ui/core'
 import { toErrorMap } from '../../util/toErrorMap'
 import { useHistory, useLocation } from 'react-router'
+import { Link } from 'react-router-dom'
 
 const Login: FC = () => {
     const [login] = useLoginMutation()
@@ -36,34 +37,47 @@ const Login: FC = () => {
         }
     })
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <h1 className="title">Login</h1>
-            <TextField
-                autoFocus
-                margin="dense"
-                name="username"
-                label="USERNAME"
-                fullWidth
-                value={formik.values.username}
-                onChange={formik.handleChange}
-                error={formik.touched.username && Boolean(formik.errors.username)}
-                helperText={formik.touched.username && formik.errors.username}
-            />
-            <TextField
-                margin="dense"
-                name="password"
-                label="PASSWORD"
-                type="password"
-                fullWidth
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                error={formik.touched.password && Boolean(formik.errors.password)}
-                helperText={formik.touched.password && formik.errors.password}
-            />
-            <button className="button link dark" type="submit">
-                LOGIN
-            </button>
-        </form>
+        <Grid container style={{ height: '100%' }}>
+            <Grid item sm={4}>
+                <div>
+                    <form onSubmit={formik.handleSubmit}>
+                        <h1 className="title">Login</h1>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            name="username"
+                            label="Username"
+                            fullWidth
+                            value={formik.values.username}
+                            onChange={formik.handleChange}
+                            error={formik.touched.username && Boolean(formik.errors.username)}
+                            helperText={formik.touched.username && formik.errors.username}
+                        />
+                        <TextField
+                            margin="dense"
+                            name="password"
+                            label="Password"
+                            type="password"
+                            fullWidth
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
+                            error={formik.touched.password && Boolean(formik.errors.password)}
+                            helperText={formik.touched.password && formik.errors.password}
+                        />
+                        <button
+                            className="button link full"
+                            type="submit"
+                            style={{ marginTop: '25px', marginBottom: '10px' }}
+                        >
+                            Login
+                        </button>
+                        <p>
+                            Not registered yet? Click <Link to="/register">here</Link> to create an account.
+                        </p>
+                    </form>
+                </div>
+            </Grid>
+        </Grid>
     )
 }
 
